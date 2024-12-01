3d554ca39049656c5a45a294e36c664ae5fe8a46
Danil Sheshenya <danil.sheshe@gmail.com>
2024-12-01 08:08:56 +0300

    remove extra files

--- src/metrics/metric.py
+++ src/metrics/metric.py
@@ -17,19 +17,7 @@ class ExampleMetric(BaseMetric):
             device (str): device for the metric calculation (and tensors).
         """
         super().__init__(*args, **kwargs)
-        if device == "auto":
-            device = "cuda" if torch.cuda.is_available() else "cpu"
         self.metric = metric.to(device)
 
-    def __call__(self, logits: torch.Tensor, labels: torch.Tensor, **kwargs):
-        """
-        Metric calculation logic.
-
-        Args:
-            logits (Tensor): model output predictions.
-            labels (Tensor): ground-truth labels.
-        Returns:
-            metric (float): calculated metric.
-        """
-        classes = logits.argmax(dim=-1)
-        return self.metric(classes, labels)
+    def __call__(self, **batch):
+        pass
